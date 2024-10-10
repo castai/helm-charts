4a8ae9e9051a94bc6251309a2e3848be7418a66b
Viktoras Makauskas <viktoras@neglostyti.com>
2024-10-10 17:49:00 +0300

    [castai-db-optimizer] bump query processor: bug fixes (#547)

--- scripts/gen-docs.sh
+++ scripts/gen-docs.sh
@@ -10,13 +10,6 @@ function show_help {
     echo "If CHART_NAME is not provided, the script will generate docs for all charts in the 'charts' directory."
 }
 
-function check_helm_docs_installed {
-    if ! command -v helm-docs &> /dev/null; then
-        echo "Error: helm-docs is not installed. Please install helm-docs and try again."
-        exit 1
-    fi
-}
-
 CHART_NAME=""
 
 while getopts "c:h" opt; do
@@ -35,13 +28,10 @@ while getopts "c:h" opt; do
     esac
 done
 
-check_helm_docs_installed
-
 if [[ -n "$CHART_NAME" ]]; then
     if [[ -d "charts/$CHART_NAME" ]]; then
         echo "Generating helm docs for chart: $CHART_NAME"
-        cd "charts/$CHART_NAME" || exit 1
-        helm-docs
+        docker run -v "./charts/$CHART_NAME":/src -w /src jnorwood/helm-docs:v1.11.0
     else
         echo "Error: Chart '$CHART_NAME' does not exist in the 'charts' directory."
         exit 1
@@ -54,8 +44,6 @@ echo "Generating helm docs for all charts in the 'charts' directory."
 for chart in charts/*; do
     if [[ -d "$chart" ]]; then
         echo "Generating helm docs for chart: $(basename "$chart")"
-        cd "$chart" || exit 1
-        helm-docs
-        cd - > /dev/null || exit 1
+        docker run -v "./$chart":/src -w /src jnorwood/helm-docs:v1.11.0
     fi
 done
